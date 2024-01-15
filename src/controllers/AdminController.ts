5111f01f599e12a0c66f279a6c055374c5fd1fef
Damian Peiris. <drpeiris3@gmail.com>
2024-01-15 12:48:31 +0530

    Added enpoints for saving and retrieving administartor details + Installed Bcrypt.

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,8 +1,9 @@
 import express, { Express, Request, Response } from "express";
-import { generateJWT } from "../auth/Auth";
+import { authenticateToken, generateJWT } from "../auth/Auth";
 import adminType from "../types/AdminType";
 import Admin from "../models/Admin";
 const adminController = express.Router();
+
 
 adminController.use(express.json());
 adminController.post('/signup',async(req:Request,res:Response)=>{
@@ -10,6 +11,37 @@ adminController.post('/signup',async(req:Request,res:Response)=>{
         let response = await Admin.create(req.body);
         return generateJWT(req, res, req.body);
         
+    } catch (error:any) {
+        res.status(500).json({message:error.message})
+        
+    }
+    
+
+})
+adminController.get('/search',authenticateToken, async(req:Request,res:Response)=>{
+    try {
+       let adminsList =  await Admin.find({email:req.query.email})
+       if(adminsList.length>0){
+           return res.status(200).json({msg:"Admin Found!",data:adminsList,isExists:true})
+
+       }else{
+        return res.status(200).json({msg:"Admin Not Found!",data:null,isExists:false})
+       }
+    } catch (error:any) {
+        res.status(500).json({message:error.message})
+        
+    }
+
+})
+adminController.delete('/delete',authenticateToken,async (req:Request,res:Response)=>{
+    try {
+        let response = await Admin.findOneAndDelete({email:req.query.email})    
+        if(response){
+            return res.status(200).json({msg:"Admin Deleted Successfully!",isDeleted:true})
+        }
+        else{
+            return res.status(200).json({msg:"Admin Not Found!",isDeleted:false})
+        }
     } catch (error:any) {
         res.status(500).json({message:error.message})
         
